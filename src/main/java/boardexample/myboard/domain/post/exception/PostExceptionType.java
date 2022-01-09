de10df61702012f91066b5a9053f2c9a2951cf57
ShinDongHun1 <huipulci@naver.com>
2022-01-09 13:24:15 +0900

    PostService delete 구현, PostSaveDto 구현

--- src/main/java/boardexample/myboard/domain/post/exception/PostExceptionType.java
+++ src/main/java/boardexample/myboard/domain/post/exception/PostExceptionType.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum PostExceptionType implements BaseExceptionType {
 
     POST_NOT_POUND(700, HttpStatus.OK, "찾으시는 포스트가 없습니다"),
-    NOT_AUTHORITY_UPDATE_POST(701, HttpStatus.OK, "포스트를 업데이트할 권한이 없습니다.");
+    NOT_AUTHORITY_UPDATE_POST(701, HttpStatus.OK, "포스트를 업데이트할 권한이 없습니다."),
+    NOT_AUTHORITY_DELETE_POST(702, HttpStatus.OK, "포스트를 삭제할 권한이 없습니다.");
 
 
     private int errorCode;
